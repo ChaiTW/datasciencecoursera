71e5aaabfd4db82c874ed7c7e49f91de22d15c6a
TIANHAN WANG <40932174+ChaiTW@users.noreply.github.com>
2018-08-03 10:48:59 +0800

    Create plot1.R

---
+++ Exploratory Data Analysis/Course Project 2/plot1.R
@@ -0,0 +1,11 @@
+## Question 1
+
+te <- aggregate(Emissions ~ year, NEIdata, sum)
+
+barplot(
+  (te$Emissions)/10^6, names.arg=te$year, xlab="Year", 
+   ylab="PM2.5 Emissions (million Tons)", main="Total PM2.5 Emissions From All US Sources"
+)
+
+#dev.copy(png,"plot1.png")
+#dev.off()
